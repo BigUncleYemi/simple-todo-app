5cd1f22d7f8078ff2d36f802bc1d20d0bbcb4491
BigUncleYemi <olayemioyebanji911@gmail.com>
2019-02-01 21:32:05 +0100

    Updates

---
+++ precache-manifest.54f99bbbabdea04bda382ec15f97fafa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-todo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b6b46e5f9654365ec95e",
+    "url": "/simple-todo-app/static/js/runtime~main.b6b46e5f.js"
+  },
+  {
+    "revision": "87135532a8de88ce198a",
+    "url": "/simple-todo-app/static/js/main.87135532.chunk.js"
+  },
+  {
+    "revision": "61b63230e5b0e76c90bd",
+    "url": "/simple-todo-app/static/js/1.61b63230.chunk.js"
+  },
+  {
+    "revision": "87135532a8de88ce198a",
+    "url": "/simple-todo-app/static/css/main.dc283808.chunk.css"
+  },
+  {
+    "revision": "3f3ffde0043da6bd0310d2428db9651d",
+    "url": "/simple-todo-app/index.html"
+  }
+];
